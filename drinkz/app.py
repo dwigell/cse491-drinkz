896be1ec156b6e1e4cedfb0234d41b327f83079e
David Wigell <wigellda@arctic.cse.msu.edu>
2013-03-27 20:23:43 -0400

    hw4 finished

--- drinkz/app.py
+++ drinkz/app.py
@@ -5,35 +5,7 @@ import simplejson
 
 import db, recipes, convert
 
-#=========================
-
-#db.add_bottle_type('Johnnie Walker', 'black label', 'blended scotch')
-#db.add_to_inventory('Johnnie Walker', 'black label', '500 ml')
-
-#db.add_bottle_type('Uncle Herman\'s', 'moonshine', 'blended scotch')
-#db.add_to_inventory('Uncle Herman\'s', 'moonshine', '5 liter')
         
-#db.add_bottle_type('Gray Goose', 'vodka', 'unflavored vodka')
-#db.add_to_inventory('Gray Goose', 'vodka', '1 liter')
-
-#db.add_bottle_type('Rossi', 'extra dry vermouth', 'vermouth')
-#db.add_to_inventory('Rossi', 'extra dry vermouth', '24 oz')
-
-#r = recipes.Recipe('scotch on the rocks', [('blended scotch', '4 oz')])
-#db.add_recipe(r)
-
-#r2 = recipes.Recipe('vomit inducing martini', [('orange juice', '6 oz'), ('vermouth', '1.5 oz')])
-#db.add_recipe(r2)
-
-#r3 = recipes.Recipe('whiskey bath', [('blended scotch', '6 liter')])
-#db.add_recipe(r3)
-
-#==================================
-
-#filename = "bin/test_database"
-
-#db.load_db(filename)
-
 
 dispatch = {
     '/' : 'index',
--- drinkz/db.py
+++ drinkz/db.py
@@ -72,23 +72,7 @@ def add_to_inventory(mfg, liquor, amount):
         new_total = float(old_amount) + float(new_amount)
         _inventory_db[(mfg, liquor)] = str(new_total)+' ml'
 
-#	if item[1]=="ml":
-#	    new_amount = item[0]
-#	    old_amount = get_liquor_amount(mfg, liquor)
-#	    new_total = float(old_amount) + float(new_amount)
-#	    _inventory_db[(mfg, liquor)] = repr(new_total)+' ml'
 
-#	if item[1]=="gallon" or item[1]=="gallons":
-#	    new_amount = convert_ml(amount)
-#	    old_amount = get_liquor_amount(mfg, liquor)
-#	    new_total = float(old_amount) + float(new_amount)
-#	    _inventory_db[(mfg, liquor)] = repr(new_total)+' ml'
-
-#       if item[1]=="liter" or item[1]=="liters":
-#          new_amount = convert_ml(amount)
-#          old_amount = get_liquor_amount(mfg, liquor)
-#          new_total = float(old_amount) + float(new_amount)
-#          _inventory_db[(mfg, liquor)] = repr(new_total)+' ml'
     else:    
     # ADD/UPDATE INVENTORY ITEM
         _inventory_db[(mfg, liquor)] =  amount
@@ -110,18 +94,7 @@ def get_liquor_amount(mfg, liquor):
     total_ml = 0.0
 
     for i in amounts:
-        total_ml += float(convert.convert_ml(i))
-    
-#    for item in amounts:
-#        item_amount = item.split()
-#        if item_amount[1]=="oz":
-#            total_ml += float(item_amount[0]) * 29.5735
-#	elif item_amount[1]=="gallon" or item_amount[1]=="gallons":
-#	    total_ml += float(item_amount[0]) * 3785.41
-#        elif item_amount[1]=="liter" or item_amount[1]=="liters":
-#            total_ml += float(item_amount[0]) * 1000
-#        else:
-#            total_ml += float(item_amount[0])    
+        total_ml += float(convert.convert_ml(i))    
 
     return total_ml 
 
@@ -130,17 +103,6 @@ def get_liquor_inventory():
     for key in _inventory_db:
         yield key[0], key[1]
 
-def convert_ml(amount):
-    total = 0.0
-
-    item = amount.split()
-    if item[1]=="oz":
-	total += float(item[0]) * 29.5735
-    if item[1]=="gallon" or item[1]=="gallons":
-        total += float(item[0]) * 3785.41
-    if item[1]=="liter" or item[1]=="liters":
-        total += float(item[0]) * 1000
-    return total
 
 def add_recipe(r):
     
