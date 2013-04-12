2bd905f82e2131611e1bd1d2f9ea58a442934c86
David Wigell <wigellda@arctic.cse.msu.edu>
2013-04-11 22:39:17 -0400

    hw5 part 1.c

--- drinkz/app.py
+++ drinkz/app.py
@@ -18,7 +18,13 @@ dispatch = {
 
     '/recipes' : 'recipes',
     '/inventory' : 'inventory',
-    '/liquorTypes' : 'liquorTypes'
+    '/liquorTypes' : 'liquorTypes',
+    '/addLiquorType' : 'addLiquorType',
+    '/recvLiquorTypes' : 'recvLiquorTypes',
+    '/addToInventory' : 'addToInventory',
+    '/recvInventory' : 'recvInventory',
+    '/addRecipe' : 'addRecipe',
+    '/recvRecipe' : 'recvRecipe'
 }
 
 html_headers = [('Content-type', 'text/html')]
@@ -68,6 +74,12 @@ alert("This is an alert box");
 <p>
 <a href='liquorTypes'>Liquor Types</a>
 <p>
+<a href='addLiquorType'>Add Liquor Type</a>
+<p>
+<a href='addToInventory'>Add Liquor to Inventory</a>
+<p>
+<a href='addRecipe'>Add Recipe</a>
+<p>
 <input type="button" onclick="alertBox()" value="Show alert box" />
 
 </body>
@@ -93,14 +105,18 @@ body {font-size: 18px;}
         data += "<b><h1>Recipes</h1></b><p>Recipe, Do We Have All the Ingredients?</p><ul>"
 
         for key in db._recipe_db:
-            a = db._recipe_db[key].ingredients[0][0]
-            b = db._recipe_db[key].ingredients[0][1]
+            data += "<p></p><li> %s,  " % (key)
+            for item in db._recipe_db[key].ingredients:
+                a = item[0]
+                b = item[1]
+                data += "%s, %s, " %(a, b)
             if len(db._recipe_db[key].need_ingredients())>0:
                 answer = "No"
-                data += """<p></p><li> %s, %s, %s, <b> %s</b><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_-xqzPnoj6Fv6aT7JeoZl7B_QsnwcfrdhuhyeZIS5SW0RutbRAg" alt="sad" width="50" height="50">""" % (key, a, b, answer)
+                data += """<b> %s</b><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_-xqzPnoj6Fv6aT7JeoZl7B_QsnwcfrdhuhyeZIS5SW0RutbRAg" alt="sad" width="50" height="50">""" % (answer)
             else:
                 answer = "Yes"
-                data += """<p></p><li> %s, %s, %s, <b> %s</b><img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcROxLShuNusDhjtK3yGGl0wT5MFKK521IWA34D8JCnTDw5Bb0fBsg" alt="happy" width="50" height="50">""" % (key, a, b, answer)
+                data +="""<b> %s</b><img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcROxLShuNusDhjtK3yGGl0wT5MFKK521IWA34D8JCnTDw5Bb0fBsg" alt="happy" width="50" height="50">""" % (answer)
+
 
         data += "</ul>"
 
@@ -171,7 +187,11 @@ body {font-size: 18px;}
         
         data += "<b><h1>Liquor Types</h1></b><p>Manufacturer, Liquor Type</p><ul>"
 
-        for mfg, liquor in db.get_liquor_inventory():
+        #for mfg, liquor in db.get_liquor_inventory():
+         #   data += "<p> </p>"
+         #   data += '<li> %s, %s' % (mfg, liquor)
+
+        for (mfg, liquor, _) in db._bottle_types_db:
             data += "<p> </p>"
             data += '<li> %s, %s' % (mfg, liquor)
 
@@ -233,7 +253,102 @@ body {font-size: 18px;}
 
         start_response('200 OK', list(html_headers))
         return [data]
+    #================================================================== HW 5 FORMS
 
+    #*** form/recv to add liquor type
+    
+    def addLiquorType(self, environ, start_response):
+        data = liquor_types_form()
+
+        start_response('200 OK', list(html_headers))
+        return [data]
+
+    def recvLiquorTypes(self, environ, start_response):
+        formdata = environ['QUERY_STRING']
+
+        results = urlparse.parse_qs(formdata)
+
+        mfg = results['man'][0]
+        name = results['name'][0]
+        typ = results['type'][0]
+
+        db.add_bottle_type(mfg, name, typ)
+        if db._check_bottle_type_exists(mfg, name):
+            print "ITS IN THE DATABASE"
+        else:
+            print "ITS NOT IN THE DATABASE"
+
+        content_type = 'text/html'
+        data = "%s <a href='/'>return to index</a>" % "Your liquor type has been added"
+
+        start_response('200 OK', list(html_headers))
+        return [data]
+
+    #*** form/recv to add to inventory
+
+    def addToInventory(self, environ, start_response):
+        data = liquor_inventory_form()
+
+        start_response('200 OK', list(html_headers))
+        return [data]
+
+    def recvInventory(self, environ, start_response):
+        formdata = environ['QUERY_STRING']
+
+        results = urlparse.parse_qs(formdata)
+
+        mfg = results['man'][0]
+        name = results['name'][0]
+        amount = results['amount'][0]
+
+        db.add_to_inventory(mfg, name, amount)
+
+        content_type = 'text/html'
+        data = "%s <a href='/'>return to index</a>" % "Your item has been added"
+
+        start_response('200 OK', list(html_headers))
+        return [data]
+
+    #*** form/recv to add recipe
+
+    def addRecipe(self, environ, start_response):
+        data = recipe_form()
+
+        start_response('200 OK', list(html_headers))
+        return [data]
+
+    def recvRecipe(self, environ, start_response):
+        formdata = environ['QUERY_STRING']
+
+        results = urlparse.parse_qs(formdata)
+
+        name = results['name'][0]
+        ingredients = results['ingredients'][0]
+
+
+        ing_list = ingredients.split(',')
+        #print ing_list
+        ing_tup_list = []
+
+        i = 0
+        while i < len(ing_list):
+            ingName = ing_list[i]
+            ingAmt = ing_list[i+1]
+            tempTup = (ingName, ingAmt)
+            ing_tup_list.append(tempTup)
+            i+=2
+
+        #print ing_tup_list
+        r = recipes.Recipe(name, ing_tup_list)
+        db.add_recipe(r)
+
+        content_type = 'text/html'
+        data = "%s <a href='/'>return to index</a>" % "Your recipe has been added"
+
+        start_response('200 OK', list(html_headers))
+        return [data]
+        
+    #================================================================== HW 5 FORMS
     def dispatch_rpc(self, environ, start_response):
         # POST requests deliver input data via a file-like handle,
         # with the size of the data specified by CONTENT_LENGTH;
@@ -303,6 +418,35 @@ def form():
     return """
 <form action='recv'>
 Amount(Please include units)<p></p> <input type='text' name='amount' size'20'>
+<input type='submit'>
+</form>
+"""
+
+def liquor_types_form():
+    return """
+<form action='recvLiquorTypes'>
+Manufacturer <input type='text' name='man' size'20'><p>
+Name <input type='text' name='name' size'20'><p>
+Type <input type='text' name='type' size'20'><p>
+<input type='submit'>
+</form>
+"""
+
+def liquor_inventory_form():
+    return """
+<form action='recvInventory'>
+Manufacturer <input type='text' name='man' size'20'><p>
+Name <input type='text' name='name' size'20'><p>
+Amount <input type='text' name='amount' size'20'><p>
+<input type='submit'>
+</form>
+"""
+
+def recipe_form():
+    return """
+<form action='recvRecipe'>
+Recipe Name <input type='text' name='name' size'20'><p>
+Ingredients(please separate with commas): ing1, amt1, ing2, amt2, ... etc)<input type='text' name='ingredients' size'20'><p>
 <input type='submit'>
 </form>
 """
